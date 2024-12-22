427508e7fdacdb5bd1e442fcbb8f9732fd4c68ba
jaehyeon <leejaehyun0312@naver.com>
2024-12-22 11:39:40 +0900

    Feat: 아이템 추가

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
+# config.py
+import math
+
 # Setting for Blocks
-# Number of blocks
 num_blocks = (5, 3)
-# Size of margin
 margin = (60, 40)
-# Size of block
 block_size = (80, 30)
-# Spacing between blocks
 spacing = (20, 20)
 score_pos = (10, 10)
 life_pos = (450, 10)
@@ -18,27 +17,25 @@ gameboard_height_coefficient = 3
 
 display_dimension = (600, 800)
 
-
 center_x = display_dimension[0] / 2
 center_y = display_dimension[1] / 2
 
-
 # Setting for paddle
 paddle_color = (242, 242, 0)
-paddle_pos = (center_x, display_dimension[1] - 100)
+paddle_pos = (center_x - 50, display_dimension[1] - 100)
 paddle_size = (100, 30)
 paddle_speed = 5
 
 # Setting for ball
 ball_color = (242, 242, 0)
 ball_speed = display_dimension[1] / 80
-ball_pos = (center_x, paddle_pos[1] - paddle_size[1])
+ball_pos = (center_x - 10, paddle_pos[1] - 30)
 ball_fever_color = (255, 50, 0)
 ball_size = (20, 20)
 
-
 # Setting for items
 item_size = (20, 20)
+item_speed = 5
 one_more_prob = 0.3
 fever_prob = 0.1
 add_score_prob = 0.1
@@ -47,7 +44,7 @@ paddle_long_prob = 0.1
 fever_time = 5
 
 add_score = 100
-add_score_color = (0, 255, 0)
+add_score_color = (255,0, 0)
 
 paddle_long_ratio = 2
 paddle_long_time = 5
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import sys
 from implements import Basic, Block, Paddle, Ball
 import config
+import random
 
 import pygame
 from pygame.locals import QUIT, Rect, K_ESCAPE, K_SPACE
@@ -37,22 +38,17 @@ def create_blocks():
 
 
 def tick():
-    global life
-    global BLOCKS
-    global ITEMS
-    global BALLS
-    global paddle
-    global ball1
-    global start
+    global life, BLOCKS, ITEMS, BALLS, paddle, ball1, start
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == K_ESCAPE:  # ESC 키가 눌렸을 때
+            if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            if event.key == K_SPACE:  # space키가 눌려지만 start 변수가 True로 바뀌며 게임 시작
+            if event.key == K_SPACE:
                 start = True
             paddle.move_paddle(event)
 
@@ -63,21 +59,33 @@ def tick():
             ball.rect.centerx = paddle.rect.centerx
             ball.rect.bottom = paddle.rect.top
 
-        ball.collide_block(BLOCKS)
+        for block in BLOCKS[:]:
+            if block.alive and ball.rect.colliderect(block.rect):
+                new_item = block.collide()  # 아이템 생성
+                if new_item:
+                    ITEMS.append(new_item)  # 생성된 아이템을 ITEMS 리스트에 추가
+                ball.dir = 360 - ball.dir + random.randint(-5, 5)
+                break  # 충돌한 블록 하나만 처리
+
         ball.collide_paddle(paddle)
         ball.hit_wall()
-        if ball.alive() == False:
+        if not ball.alive():
             BALLS.remove(ball)
 
+    # 아이템 이동 및 충돌 처리
+    for item in ITEMS[:]:
+        item.move()
+        if item.rect.colliderect(paddle.rect):
+            if item.type == "red":
+                life += 1  # 점수 추가
+            elif item.type == "blue":
+                paddle.rect.width *= config.paddle_long_ratio  # 패들 크기 증가
+            ITEMS.remove(item)
+        elif item.rect.top > config.display_dimension[1]:
+            ITEMS.remove(item)
 
 def main():
-    global life
-    global BLOCKS
-    global ITEMS
-    global BALLS
-    global paddle
-    global ball1
-    global start
+    global life, BLOCKS, ITEMS, BALLS, paddle, ball1, start
     my_font = pygame.font.SysFont(None, 50)
     mess_clear = my_font.render("Cleared!", True, config.colors[2])
     mess_over = my_font.render("Game Over!", True, config.colors[2])
@@ -91,8 +99,10 @@ def main():
         for block in BLOCKS:
             block.draw(surface)
 
-        cur_score = config.num_blocks[0] * config.num_blocks[1] - len(BLOCKS)
+        for item in ITEMS:  # 아이템 그리기
+            item.draw(surface)
 
+        cur_score = config.num_blocks[0] * config.num_blocks[1] - len(BLOCKS)
         score_txt = my_font.render(f"Score : {cur_score * 10}", True, config.colors[2])
         life_font = my_font.render(f"Life: {life}", True, config.colors[0])
 
@@ -111,11 +121,7 @@ def main():
             surface.blit(mess_clear, (200, 400))
         else:
             for ball in BALLS:
-                if start == True:
-                    ball.move()
                 ball.draw(surface)
-            for block in BLOCKS:
-                block.draw(surface)
 
         pygame.display.update()
         fps_clock.tick(config.fps)
